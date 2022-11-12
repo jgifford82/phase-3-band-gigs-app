dc8bedd796751af61cc7ebcf38f288aa71121bec
Jennifer Gifford <jgifford82@gmail.com>
2022-11-12 16:35:28 -0700

    set up method for seeding cities

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,17 @@ puts "🌱 Seeding data..."
 
 # Seed your database here
 
+# Create 10 cities
+10.times do
+    city = City.create(
+        name: Faker::Address.city,
+    )
+
+#For each city, create 5 events
+# 5.times.do
+#     item = Item.create(
+#         band: Faker::
+#     )
+# end
+
 puts "✅ Done seeding!"
