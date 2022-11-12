ec98a74861a2b16064456eb1d6becf08c3b2e4ca
Jennifer Gifford <jgifford82@gmail.com>
2022-11-12 11:46:26 -0700

    add belongs_to macro to Event class

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
+# belongs_to must be singular for the macro to work properly, otherwise the inferred class will be wrongly pluralized too.
 class Event < ActiveRecord::Base
+    belongs_to :city
 end
