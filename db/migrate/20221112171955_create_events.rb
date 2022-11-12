7fa718aed246a5b8a4c236cc3cc558d61a325b8a
Jennifer Gifford <jgifford82@gmail.com>
2022-11-12 11:05:45 -0700

    set up events migration table

--- db/migrate/20221112171955_create_events.rb
+++ db/migrate/20221112171955_create_events.rb
@@ -1,4 +1,15 @@
 class CreateEvents < ActiveRecord::Migration[6.1]
   def change
+    create_table :events do |t|
+      t.string :band
+      t.string :venue
+      t.date :date
+      t.time :time
+      t.integer :price
+      # foreign id to connect which city an event belongs to:
+      t.integer :city_id
+      # timestamps creates 2 columns: created_at & updated_at:
+      t.timestamps
+    end
   end
 end
