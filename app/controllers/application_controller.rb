b841a647371edfdda60a0a2c24d927b8a27e0bdf
Jennifer Gifford <jgifford82@gmail.com>
2022-11-19 14:37:29 -0700

    comments out all routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,9 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
-  end
+  # get "/" do
+  #   { message: "Good luck with your project!" }.to_json
+  # end
 
   # This works:
   # get "/cities" do
@@ -17,11 +17,11 @@ class ApplicationController < Sinatra::Base
   #   cities.to_json
   # end
 
-    # This works to show all cities including nested events data for each city:
-    # get "/cities" do
-    #   City.all.to_json(include: :events)
-    # end
+  # This works to show all cities including nested events data for each city:
+  # get "/cities" do
+  #   City.all.to_json(include: :events)
+  # end
 
-  # test code for commit
+# test code for commit
 
 end
