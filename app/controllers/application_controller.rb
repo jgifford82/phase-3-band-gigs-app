f6bfbf81b89a25862416c4ff7c566e0651ff873f
Jennifer Gifford <jgifford82@gmail.com>
2022-11-21 10:38:59 -0700

    set up get cities route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,9 @@ class ApplicationController < Sinatra::Base
   # end
 
   # This works to show all cities including nested events data for each city:
-  # get "/cities" do
-  #   City.all.to_json(include: :events)
-  # end
+  get "/cities" do
+    City.all.to_json(include: :events)
+  end
 
 # test code for commit
 
