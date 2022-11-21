048f3c69436df2effc1dc2e67e593bb99fc41766
Jennifer Gifford <jgifford82@gmail.com>
2022-11-21 11:21:22 -0700

    set up cities post route

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,4 +3,11 @@ class CitiesController < ApplicationController
     get "/cities" do
       City.all.to_json(include: :events)
     end
+
+    post "/cities" do 
+        City.create(
+            name: params[:name]
+        ).to_json
+    end
+
 end
