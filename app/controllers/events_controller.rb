7fde840b09407309b0ae61c0c89cfca15da39b38
Jennifer Gifford <jgifford82@gmail.com>
2022-11-21 11:35:08 -0700

    sets up events post route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,16 @@ class EventsController < ApplicationController
       Event.all.to_json
     end
 
+    # Add a new event
+    post "/events" do 
+        Event.create(
+            band: params[:band],
+            venue: params[:venue],
+            date: params[:date],
+            time: params[:time],
+            price: params[:price],
+            city_id: params[:city_id],
+        ).to_json
+    end
+
 end
