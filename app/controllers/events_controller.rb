0c8a9f7747155d06b6ee24569d2e18d24f42206d
Jennifer Gifford <jgifford82@gmail.com>
2022-11-21 10:34:40 -0700

    commenting out route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
-    get "/events" do
-    #   Event.all.to_json
-    "Hello World"
-    end
+    # get "/events" do
+    # #   Event.all.to_json
+    # "Hello World"
+    # end
 end
